4335897ae1560caaf69032709efea33b41cf4cf6
luk1pr0 <lpytel16@gmail.com>
2024-09-07 20:54:27 +0100

    Adding content types to CMS

---
+++ src/api/payment-page/routes/payment-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::payment-page.payment-page');
---
+++ src/api/payment-page/services/payment-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::payment-page.payment-page');
